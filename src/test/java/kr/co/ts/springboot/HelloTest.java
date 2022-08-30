17583b749c717753a9d00ad002906980c81ecb30
Ruiez <hi.moonlight179@gmail.com>
2022-08-30 14:08:02 +0900

    Initial commit

---
+++ src/test/java/kr/co/ts/springboot/HelloTest.java
@@ -0,0 +1,29 @@
+package kr.co.ts.springboot;
+
+import kr.co.ts.springboot.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void helloReturns() throws Exception{
+        String msg = "Hello";
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello"));
+    }
+}
